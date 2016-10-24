97ecbe8b06cb41eb4ba0907ca624f7dbeee77efc
philscanlon <phil.scanlon@gmail.com>
2016-10-24 15:06:37 +0800

    Create pub_fo_many_to_one_direct_througput.sh

---
+++ pub_fo_many_to_one_direct_througput.sh
@@ -0,0 +1,9 @@
+no_of_pub=$1
+no_of_topics=$3
+for ((i=1;i<=$3;i++))
+do
+  for((j=1;j<=no_of_pub;j++))
+  do
+taskset -c 1-20  nohup ./sdkperf_java.sh  -cip=10.191.247.245 -ptl=test/fo/$i -mn=1000000000 -mr=2500000  -cu=nomura-du@nomura  -cp=password -msa=$2 -psm -q >/dev/null 2>&1 &
+  done
+done
