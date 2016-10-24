9789bab33e58e43d8ab37f536cc13936cc892944
philscanlon <phil.scanlon@gmail.com>
2016-10-24 15:00:04 +0800

    Create 10MAggTopics.sh

---
+++ 10MAggTopics.sh
@@ -0,0 +1,3 @@
+./sdkperf_java.sh -cip=10.191.247.245  -stf=5MTopics.txt -cu=nomura-du@nomura  -cp=password -q  >/dev/null 2>&1  &
+./sdkperf_java.sh -cip=10.191.247.245  -stf=5MTopics.txt -cu=nomura-du@nomura  -cp=password -q  >/dev/null 2>&1  &
+
